27366bc039e33149ef633dad12753af3b720fdda
Luka Napotnik <luka.napotnik@gmail.com>
2013-11-02 23:17:40 +0100

    use mongodb

--- src/goready/db.go
+++ src/goready/db.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"labix.org/v2/mgo"
+	_ "labix.org/v2/mgo/bson"
+)
+
 // Deployment stores all infomration of a remote deployment
 type Deployment struct {
 	Name           string
@@ -7,14 +12,8 @@ type Deployment struct {
 	TargetHost     string
 	DeploymentKey  string
 	Notes          string
-	Project struct {
-		Name     string
-		Version  string
-		VCSHost  string
-		VCSName  string
-	}
 	Options struct {
-		AutoUpdate bool
+		AutoUpdate  bool
 		ValidBranch string
 	}
 }
@@ -35,10 +34,43 @@ func (d *Deployment) Start() (string, error) {
 	return "", nil
 }
 func (d *Deployment) Stop(reason string) error {
-	return "", nil
+	return nil
 }
 
 func (d *Deployment) Restart(reason string ) error {
-	return "", nil
+	return nil
 }
 
+func LoadDeployments() ([]Deployment, error) {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("goready").C("deployments")
+	result := []Deployment{}
+	err = c.Find(nil).All(&result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func InsertDeployments() error {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("goready").C("deployments")
+	err = c.Insert(
+		&Deployment{"Testna", "Testna postavitev", "localhost", "127.0.0.1", "AAABBB", struct{AutoUpdate bool; ValidBranch string}{false, "master"}},
+		&Deployment{"Druga", "Druga testna postavitev", "localhost", "127.0.0.1", "CCCDDD", struct{AutoUpdate bool; ValidBranch string}{true, "unstable"}},
+		&Deployment{"Beta staging", "Beta staging server", "localhost", "127.0.0.1", "CCCDDD",struct{AutoUpdate bool; ValidBranch string}{false, "master"}})
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- src/goready/main.go
+++ src/goready/main.go
@@ -4,28 +4,22 @@ import (
 	"github.com/hoisie/web"
 	"encoding/json"
 	"fmt"
-	"time"
 	"net/http"
 	"html/template"
 )
-
-type Deployment struct {
-	Name           string
-	Description    string
-	Hostname       string
-	IPAddress      string
-	deploymentKey  string // deploymeny key
-	Configuration  string // custom configuration in json
-}
 
 func VerifyDeploymentKey() bool {
 	return true
 }
 func GetDeployments(ctx *web.Context) {
-	deployments := []*Deployment{
-		&Deployment{"Testna", "Testna postavitev", "localhost", "127.0.0.1", "AAABBB", ""},
-		&Deployment{"Druga", "Druga testna postavitev", "localhost", "127.0.0.1", "CCCDDD", ""},
-		&Deployment{"Beta staging", "Beta staging server", "localhost", "127.0.0.1", "CCCDDD", ""},
+	fmt.Printf("************************************\n")
+	deployments, err := LoadDeployments()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, deployment := range(deployments) {
+		fmt.Printf(" Deployment %s ...\n", deployment.Name)
 	}
 	b, err := json.Marshal(deployments)
 	if err != nil {
@@ -35,49 +29,21 @@ func GetDeployments(ctx *web.Context) {
 	ctx.Write(b)
 }
 
-func GetStatusUpdates(ctx *web.Context) {
-	deployments := []*Deployment{
-		&Deployment{"Testna", "Testna postavitev", "localhost", "127.0.0.1", "AAABBB", ""},
-		&Deployment{"Druga", "Druga testna postavitev", "localhost", "127.0.0.1", "CCCDDD", ""},
-		&Deployment{"Beta staging", "Beta staging server", "localhost", "127.0.0.1", "CCCDDD", ""},
-	}
-	b, err := json.Marshal(deployments)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	ctx.ContentType("json")
-	ctx.Write(b)
-}
 func GetIndex(ctx *web.Context) {
 	t, _ := template.ParseFiles("static/index.html")
 	t.Execute(ctx.ResponseWriter, nil)
 }
 
-func wsHandler(ws *websocket.Conn) {
-	op := struct {
-		Type string `json:type`
-		Data struct {
-			Name string
-			Operation string
-		} `json:data`
-	}{}
-
-	websocket.JSON.Receive(ws, &op)
-
-	switch op.Data.Operation {
-	case "upgrade":
-		fmt.Printf("Performing upograde of deploymeny '%s' ...\n", op.Data.Name)
-	}
-	websocket.JSON.Send(ws, map[string]string{"type":"message", "data":"OK"})
-	time.Sleep(time.Second * 2)
-	websocket.JSON.Send(ws, map[string]string{"type":"message", "data":"DONE"})
-	ws.Close()
-}
 func main() {
+
+	// Uncomment this to fill the database with dummy values
+	/*err := InsertDeployments()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}*/
 	web.Get("/static", http.FileServer(http.Dir("static")))
-	web.Get("/ws", websocket.Handler(wsHandler))
 	web.Get("/", GetIndex)
-	web.Get("/status", GetStatusUpdates)
 	web.Get("/deployments", GetDeployments)
 	web.Run("0.0.0.0:9999")
 }
